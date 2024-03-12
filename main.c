b792c764a72e9f3d5f1521c686dba4b2bff782ef
neoks <neokspp@proton.me>
2024-03-12 18:36:14 -0500

    Prueba y estructuras

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+//Contadores
+int i = 0;
+
+int main(){
+	printf("Hola, yo, esto es Crimecity, ahora no sabes qué hacer,\n");
+		printf("Pero quieres por ahora, crear algo que genere por si sólo, lo siguiente\n");
+		printf("1. Guarde la información de un área,\n");
+		printf("2. Sea capaz de particionar dicha área en sub áreas no necesariamente iguales,\n");
+		printf("3. Las sub áreas están separadas por la carretera, la cual está seccionada en calles,\n");
+		printf("4. La carretera es un conjunto continuo, abierto\n");
+		printf("Por ahora, sera una funcion para calcular raiz cuadrada y precedida por una de descomposicion de digitos\n");
+
+		int numero;
+		scanf("%d", &numero);
+		int aux = 0;		
+		while( aux != numero){
+			i++;
+			aux = numero - i
+			
+		}
+	return 0;
+}
