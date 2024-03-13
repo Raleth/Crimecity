fa61827c7230dcacf88210bc48010c9d1d8c5d9a
Raleth <dalonvia@gmail.com>
2024-03-12 21:21:37 -0500

    hola mundo en c

---
+++ holamundo.c
@@ -0,0 +1,11 @@
+#include <stdio.h>  //librerias estandar de entrada y salida
+
+int main(){
+
+    printf("hola mundo en lenguaje C\n"); //el prinft en C es el equivalente del 
+                                            //cout en C++
+
+    return 0;
+
+}
+
